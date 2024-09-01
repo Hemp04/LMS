240b103174393d2f66198c7b5c40299a2745d470
Hemp04 <hbparikh4@gmail.com>
2024-09-01 12:10:39 +0530

    Book's AuthorName and TitleName Validation is refactored

--- src/main/java/org/example/LibraryManagementSystem.java
+++ src/main/java/org/example/LibraryManagementSystem.java
@@ -38,12 +38,7 @@ public class LibraryManagementSystem {
         if (book == null) {
             throw new IllegalArgumentException("Book cannot be null.");
         }
-        if (book.getTitle() == null || book.getTitle().trim().isEmpty()) {
-            throw new IllegalArgumentException("Book title cannot be null or blank.");
-        }
-        if (book.getAuthor() == null || book.getAuthor().trim().isEmpty()) {
-            throw new IllegalArgumentException("Book Author cannot be null or blank.");
-        }
+
         if (book.getISBN() == null || book.getISBN().trim().isEmpty()) {
             throw new IllegalArgumentException("Book ISBN No. cannot be null or blank.");
         }
@@ -53,13 +48,17 @@ public class LibraryManagementSystem {
         if (book.getISBN().length() != 16 ) {
             throw new IllegalArgumentException("Book ISBN No. Invalid ");
         }
+
         for (Books availableBook : availableBooks) {
             if (availableBook.getISBN().equals(book.getISBN())) {
                 throw new IllegalArgumentException("Book cannot be added as there is already a book added with ISBN: " + book.getISBN());
             }
         }
-        availableBooks.add(book);
-        System.out.println("Book with ISBN " + book.getISBN() + " added successfully!");
+        if(validateAuthor(book.getAuthor()) && validateTitle(book.getTitle())){
+            availableBooks.add(book);
+            System.out.println("Book with ISBN " + book.getISBN() + " added successfully!");
+        }
+
     }
     public void borrowBook(String ISBN) throws IllegalArgumentException{
         // Using Iterator to safely and efficiently remove the book from the list while iterating it at the same time
@@ -89,6 +88,18 @@ public class LibraryManagementSystem {
             }
         }
         throw new IllegalArgumentException("Book with ISBN No. " + ISBN + " can not be returned as it is not yet Borrowed");
+    }
+    private boolean validateAuthor(String author) throws IllegalArgumentException {
+        if (author == null || author.trim().isEmpty()) {
+            throw new IllegalArgumentException("Book Author cannot be null or blank.");
+        }
+        return true;
+    }
+    private boolean validateTitle(String title) throws IllegalArgumentException {
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Book Title cannot be null or blank.");
+        }
+        return true;
     }
 
     }
